4de16bedf477bfb1a8a05f82b7ea8c00abf86df8
DESKTOP-8857DGO\503 <audelahd@gmail.com>
2023-06-14 16:55:25 +0900

    ArticleDAO 기사작성, 기사 정보 DB에서 불러오는 액션 (수정중)

---
+++ news/src/main/java/article/ArticleDAO.java
@@ -0,0 +1,113 @@
+package article;
+
+import java.sql.Blob;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+public class ArticleDAO {
+	private PreparedStatement pstmt;
+	private Connection conn;
+	private DataSource dataFactory;
+	
+	public ArticleDAO() {
+		try {
+			Context ctx = new InitialContext();
+			Context envCtx = (Context) ctx.lookup("java:/comp/env");
+			dataFactory = (DataSource) envCtx.lookup("jdbc/oracle");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public List<ArticleVO> selectAllArticles(){
+		System.out.println("selectAllArticles 들어옴");
+		List<ArticleVO> articlesList = new ArrayList<ArticleVO>();
+		
+		try {
+			conn=dataFactory.getConnection();
+			String query = "SELECT title, content, articlenum,type, reccount, hotissue, id "+
+			" FROM ARTICLE ORDER by ARTICLENUM DESC";
+			
+			System.out.println(query);
+			pstmt = conn.prepareStatement(query);
+			ResultSet rs = pstmt.executeQuery();
+			
+			while(rs.next()) {
+				String title = rs.getString("title");
+				String content = rs.getString("content");
+				int articleNum = rs.getInt("articlenum");
+				int type = rs.getInt("type");
+				int reccount = rs.getInt("reccount");
+				int hotissue = rs.getInt("hotissue");
+				String id = rs.getString("id");
+				
+				ArticleVO article = new ArticleVO();
+				article.setTitle(title);
+				article.setContent(content);
+				article.setArticlenum(articleNum);
+				article.setType(type);
+				article.setRecCount(reccount);
+				article.setHotissue(hotissue);
+				article.setId(id);
+				articlesList.add(article);
+				
+			}
+			
+			rs.close();
+			pstmt.close();
+			conn.close();
+			
+			
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		
+		return articlesList;
+	}
+	
+
+	
+	public void insertNewArticle(ArticleVO article) {
+		try {
+			
+			System.out.println("insertNewArticle메소드");
+			conn = dataFactory.getConnection();
+			String title = article.getTitle();
+			String content = article.getContent();
+			int type = article.getType();
+			int reccount = article.getRecCount();
+			int hotissue = article.getHotissue();
+			String img = article.getimgFileName();
+			String id = article.getId();
+			
+			String query = "INSERT INTO ARTICLE (title, writedate, updatedate, content, articlenum, type, reccount, hotissue, img, id)"
+					+ " values(?, sysdate, sysdate, ?, seq_anum.nextval, ?, 0, ?, null, 'reporter1')";
+			
+			System.out.println(query);
+			pstmt=conn.prepareStatement(query);
+			pstmt.setString(1, title);
+			pstmt.setString(2, content);
+			pstmt.setInt(3, type);
+			pstmt.setInt(4, hotissue);
+			
+			
+			pstmt.executeUpdate();
+			pstmt.close();
+			conn.close();
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		
+	}
+}
