49d151dff3de086c76b5e2e9580ebda062bfa6b2
DESKTOP-8857DGO\503 <audelahd@gmail.com>
2023-06-16 20:44:08 +0900

    DAO 반응관련 추가 및 수정

--- news/src/main/java/article/ArticleService.java
+++ news/src/main/java/article/ArticleService.java
@@ -6,17 +6,33 @@ public class ArticleService {
 	
 	ArticleDAO articleDAO;
 	
+	
 	public ArticleService() {
 		articleDAO = new ArticleDAO();
 	}
 	
 	public void addArticle (ArticleVO article) {
 		articleDAO.insertNewArticle(article);
+		
+	}
+	
+	public void addReact(ArticleVO article) {
+		articleDAO.firstAddReact(article);
+	}
+	
+	public ArticleVO updateAction(ArticleVO article) {
+		articleDAO.updateReact(article);
+		return article;
 	}
 	
 	public List<ArticleVO> listArticles(){
 		List<ArticleVO> articlesList = articleDAO.selectAllArticles();
 		return articlesList;
+	}
+	
+	public void viewArticle(ArticleVO article) {
+		
+		articleDAO.selectArticle(article);
 	}
 
 }
